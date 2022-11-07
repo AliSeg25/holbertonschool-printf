031f5d9ca1fe52611f6130c32561e17861946f29
Your Name <as.segueg2501@gmail.com>
2022-11-07 00:47:25 -0800

    main.h

--- main.h
+++ main.h
@@ -5,6 +5,13 @@
 #include <stdio.h>
 #include "main.h"
 
+typedef struct app_func
+{
+	char *type;/* int, char, string */
+	void (*f)(va_list);
+} type;
+
+
 int _printf(const char *format, ...);
 
 

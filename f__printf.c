339782702d3d92d60f91a7eea612c58452aa0501
Your Name <as.segueg2501@gmail.com>
2022-11-08 10:28:57 -0800

    essay

--- f__printf.c
+++ f__printf.c
@@ -12,14 +12,8 @@ int f__printf(const char *format, t appfunc[], va_list arg)
 	int j;
 	int longue = 0;
 
-	if (format [0] == '%' && format [1] == '\0')
+	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
-
-	if (format [0] == '%' && (format [1] >= 33 || format [1] <= 122))
-	{
-		my_write(format[0]);
-		my_write(format[1]);
-	}
 
 	for (i = 0; format[i] != '\0'; i++)
 	{

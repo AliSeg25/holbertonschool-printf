bafc8916490733e591e0c2c85cdd44e3d81c9664
Your Name <as.segueg2501@gmail.com>
2022-11-08 06:55:57 -0800

    M

--- f__printf.c
+++ f__printf.c
@@ -22,10 +22,6 @@ int f__printf(const char *format, t appfunc[], va_list arg)
                 if (format[i + 1] == *appfunc[j].type)
                 {
                         c  = appfunc[j].f(arg);
-
-			if (format[i + 1] == '%')
-				return (-1);
-			
                         longue += c;
                         break;
                 }

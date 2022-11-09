38e16de539bb507192c26f8ff5168a0bae8e1180
CllaudiaB <buraanna27@gmail.com>
2022-11-09 03:52:22 -0800

    test

--- print_int.c
+++ print_int.c
@@ -10,14 +10,23 @@ int print_int(va_list arg)
 {
 	int num;
 	int len = 0;
+	int div = 1;
 
 	num = va_arg(arg, int);
 
 	if (num < 0)
 	{
 		len += my_write('-');
+		num *= -1;
 
 	}
-	len += my_write(num + '0');
+	while ((num / div) > 9)
+		div *= 10;
+	while (div > 0)
+	{
+		len += my_write('0' + (num / div));
+		num %=  div;
+		div /= 10;
+	}
 	return (len);
 }

58e5ad7cc28d00511f53df8335dba1a311c010e0
CllaudiaB <buraanna27@gmail.com>
2022-11-09 23:07:26 -0800

    function print_int

--- print_int.c
+++ print_int.c
@@ -9,17 +9,16 @@
  */
 int print_int(va_list arg)
 {
-	int num;
+	long int num;
 	int len = 0;
-	int div = 1;
+	long int div = 1;
 
 	num = va_arg(arg, int);
 
-	if (num < 0)
+	if (num < 0 )
 	{
 		len += my_write('-');
 		num *= -1;
-
 	}
 	while ((num / div) > 9)
 		div *= 10;
